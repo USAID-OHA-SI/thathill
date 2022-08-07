67617ffa56d320e4fc13024ba32b25545bd895f8
tessam30 <tim.essam@gmail.com>
2022-08-07 12:11:51 -0400

    feat:needs a lot of tidying, but attempt 1 at clustering the data

--- Scripts/explore_unaids_te.R
+++ Scripts/explore_unaids_te.R
@@ -17,6 +17,7 @@ library(ggplot2)
 library(googlesheets4)
 library(assertr)
 library(extrafont)
+library(scales)
 
 # Set paths  
 data   <- "Data"
@@ -129,6 +130,24 @@ df_viz %>%
 
 # Strip plots with CIs
 # Let's move to a heatmapish type view
+# Reflect axis on RHS
+ou_order_rhs <- df_viz %>% 
+  clean_countries("country") %>% 
+  filter(year == ymax,
+         full_cascade == 1) %>% 
+  mutate(ou_order = fct_reorder(country, ou_order_val)) 
+ou_order_rhs <- levels(ou_order_rhs$ou_order)
+
+ou_order_rhs_labs <- df_viz %>% 
+  clean_countries("country") %>% 
+  filter(year == ymax,
+         full_cascade == 1) %>% 
+  mutate(ou_order = fct_reorder(country, ou_order_val)) %>% 
+  distinct(ou_order)
+
+
+
+
 df_viz %>% 
   clean_countries("country") %>% 
   filter(year == ymax,
@@ -139,8 +158,141 @@ df_viz %>%
   geom_vline(xintercept = 95, linetype = "dotted") +
   geom_segment(aes(x = lb, xend = ub, yend = ou_order), size = 1, lineend = "butt",
                color = grey50k) +
-  #geom_point(aes(x = est)) +
+  #geom_point(aes(x = est, color = ifelse(est >= 95, scooter, old_rose))) +
   facet_wrap(~ind_facet) +
-  si_style_xgrid()
+  #scale_color_identity() +
+  scale_x_continuous(label = label_number(suffix = "%"))+
+  si_style_xgrid() +
+  labs(caption = glue::glue("UNAIDS 2022 Data | {ref_id}"), x = NULL, y = NULL,
+       title = "NO PEPFAR SUPPORTED OPERATING UNIT HAS REACHED 95-95-95 AS OF 2021")
 
+  si_save("Graphics/UNAIDS_95s_2022_error_bars.svg")
+  
 
+# CLUSTERING --------------------------------------------------------------
+
+  # What if we incorporate an additional metric into the mix that captures how large
+  # the error measurement is
+  
+  # % of error associated with country?
+  # Let's start with a simple method, we'll take the point estimate - min value
+  
+  # How do the OUs cluster?
+  df_clust <- df_viz %>% 
+    clean_countries("country") %>% 
+    filter(year == ymax,
+           full_cascade == 1) %>% 
+    mutate(error_var = est - lb) %>% 
+    select(country, est, ind_facet, error_var) 
+  
+  # Split table apart then reappend
+  tmp1 <- df_clust %>% 
+    select(country, ind_facet, est = error_var) %>% 
+    mutate(ind_facet = str_c(ind_facet, "_err"))
+    
+  df_clust_error <- 
+    df_clust %>% 
+    select(-error_var) %>% 
+    bind_rows(tmp1) %>% 
+    spread(ind_facet, est)
+
+  set.seed(41)
+  mod_clust <-  kmeans(select(df_clust_error, -country), centers = 5, )    
+  summary(mod_clust)  
+
+  library(broom)
+  tidy(mod_clust)
+
+  # Augment back results to data
+  
+  df_3d <- augment(mod_clust, df_clust_error)
+  augment(mod_clust, df_clust_error) %>% 
+    ggplot(aes(`KNOWN STATUS`-thresh, `KNOWN STATUS ON ART`-thresh, color = .cluster)) + 
+    annotate("rect", fill = grey10k, xmin = -5, xmax = 5, 
+             ymin = -5, ymax = 5, alpha = 0.5) +
+    geom_vline(xintercept = -5, linetype = "dotted", color = glitr::grey40k) +
+    geom_hline(yintercept = -5, linetype = "dotted", color = glitr::grey40k) +
+    geom_point(aes(size = abs(`ON ART VLS`-thresh))) +
+    ggrepel::geom_text_repel(aes(label = country), size = 8/.pt) +
+    scale_size_binned() +
+    scale_color_discrete() +
+    si_style() +
+    labs(x = "Gap to Known Status Goal", y = "Gap to ART Goal", 
+         size = "Gap to VLS Goal",
+         color = "Cluster",
+         title = "SAMPLE CLUSTER ANALYSIS OF 95s CASCADE",
+         caption = glue::glue("Source: UNAIDS 2022 | {ref_id}")) +
+    guides(color = guide_legend(nrow = 2, byrow = TRUE))
+  
+  ggsave("Images/UNAIDS_cascade_cluster.png", height = 8, width = 10.5)
+
+  
+
+  
+  # PCA approach
+  pca_fit <- df_clust_error %>% 
+    select(-country) %>% 
+    prcomp(scale = T)
+  
+  
+  pca_fit %>%
+    augment(df_clust_error) %>% # add original dataset back in
+    ggplot(aes(.fittedPC1, .fittedPC2)) + 
+    geom_point(size = 1.5) +
+    geom_vline(xintercept = 0) +
+    geom_hline(yintercept = 0) +
+    ggrepel::geom_label_repel(aes(label = country)) 
+
+  
+  library(plotly)
+  
+  tmp <- plot_ly(data = df_3d, x = ~`KNOWN STATUS`, y = ~`KNOWN STATUS ON ART`,
+          z = ~`ON ART VLS`, type = "scatter3d", text = ~country, color = ~.cluster )
+  tmp
+  
+  # prep data for cluster dd
+  thresh <- 95
+  df_dd <- df_3d %>% 
+    column_to_rownames("country") %>% 
+    select(-.cluster) %>% 
+    mutate(across(1:3, ~ .x - thresh))
+  
+  # cluster dendogram
+  dd <- dist(scale(df_dd), method = "euclidean")
+  hc <- hclust(dd, method = "ward.D2")
+  plot(hc)
+  rect.hclust(hc, k = 5, border = 1:5)
+  
+  #convert into dendogram
+  hcd <- as.dendrogram(hc)
+  plot(hcd, horiz = TRUE)  
+  
+  # ggplot2 method
+  library(ggdendro)
+  cut_hc <- cutree(hc, k = 6)
+
+  hcdata <- dendro_data(hc, type = "rectangle")  
+  
+  ggplot() +
+    geom_segment(data = segment(hcdata), 
+                 aes(x = x, y = y, xend = xend, yend = yend)
+    ) +
+    geom_text(data = label(hcdata), 
+              aes(x = x, y = y, label = label, hjust = 0), 
+              size = 3
+    ) +
+    coord_flip() +
+    scale_y_reverse(expand = c(0.2, 0))
+  
+  
+  library(dendextend)
+ 
+  
+  dend <- as.dendrogram(hc)
+  plot(dend)
+  dend2 <- cut(dend, h = 3)
+  plot(dend2$upper)
+  
+  dend  
+  dend %>% head  
+  
