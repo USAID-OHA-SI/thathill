f1e1fc6c33ad0ee56d0876ed3b128b23cf3297f5
jhoehner <jhoehner@usaid.gov>
2022-10-03 12:22:55 -0400

    removed code associated with initial analytic questions
    questions were answered and were changed

--- Scripts/explore_kp.R
+++ Scripts/explore_kp.R
@@ -4,7 +4,7 @@
 # REF ID:   d1836592
 # LICENSE:  MIT
 # DATE CREATED: 2022-07-21
-# DATE UPDATED: 2022-09-02
+# DATE UPDATED: 2022-09-07
 
 # dependencies -----------------------------------------------------------------
 
@@ -317,6 +317,10 @@ missing_data_heatmap <- function(df, title){
 indicator_crosswalk <- read_sheet("1iv5aBHXSqO2Ky4d6zEORl2KwL_7_08w_D0CSTKgoq0A") %>%
   clean_names()
 
+# pepfar countries 
+
+pepfar_countries <- pepfar_country_list
+
 # - KP data
 kp_data <- read_sheet("1KORmN23RjAKDz9yroJA5yn9csBOGBKDHKmK45JIs7tY")
   
@@ -369,245 +373,33 @@ write_sheet(kp_tidier, "1Jxj2PlJKSr_LrU2uNs-DBtl-6hwmzaha6Gg6wDQuAxw",
 # focusing especially on PEPFAR supported OUs
 
 # How many unique national OUs are in the data?
-
 unique_ous <- kp_tidier %>%
   select(area, area_id) %>%
   distinct() 
-# 159
-
-# how many pepfar supported OUs?
-pepfarunique_ous <- unique_ous %>%
-  drop_na(operatingunit)
-# 55
-
-# for PSE
-
-ou_table_pse <- as.data.frame(tabyl(kp_tidier, time_period, population)) %>%
-  pivot_longer(!time_period, 
-               names_to = "population", values_to = "n_estimates") %>%
-  filter(n_estimates > 0) %>%
-  group_by(time_period, population)
-
-ggplot(ou_table_pse, 
-       aes(x = time_period, 
-           y = n_estimates, 
-           group = population, 
-           fill = population)) + 
-  geom_col() +
-  facet_wrap(~population) +
-  scale_fill_manual(values = c(old_rose_light, denim_light, golden_sand_light, 
-                                genoa_light, moody_blue_light)) +
-  si_style_ygrid() +
-  labs(
-    y = "Number of OUs with Population Size Estimate")
-
-# we can see that there is only at least one estimate available for any 
-# KP between 2011-2020
-# the maximum number of OUs represented for a given KP 
-# in a given year is 50 and this only occurs for 2016 and 2019 for PSEs for sex workers
-# In most years, for most KPs there are fewer than 30 OUs with PSE data for a
-# given year and KP so for most years we are only seeing PSE data for a given KP
-# for 30/159 = 19% of all OUs represented in the data and if we assume all 30 were 
-# pepfar supported OUs, that would still only be data from 54% of all pepfar 
-# supported OUs present in the data.
-
-# for coverage
-
-complete_totals_cov <- kp_tidier %>%
-  filter(geo_level == "National",
-         indicator_new %in% c("Coverage of HIV prevention programmes"))
 
 # Which ous have complete PSE data for all KPs?
-complete_totals_2020_pse <- complete_totals_2020 %>%
-  filter(indicator_new == "Population Size Estimate")
-
-ou_table_2020_pse <- as.data.frame(tabyl(complete_totals_2020_pse, area, population)) %>%
-  pivot_longer(!area, names_to = "population", values_to = "n_estimates") %>%
-  filter(n_estimates > 0) %>%
-  group_by(area) %>%
-  summarize(n_kps = sum(n_estimates)) %>%
-  filter(n_kps >= 5)
-
-complete_totals_2020 <- kp_tidier %>%
-  filter(time_period == "2020", 
-         geo_level == "National",
-         subgroup %in% c("Total", "estimate"),
-         indicator_new %in% c("Population Size Estimate", 
-                              "Coverage of HIV prevention programmes"))
-
-# Which ous have complete PSE data for all KPs?
-complete_totals_2020_pse <- complete_totals_2020 %>%
-  filter(indicator_new == "Population Size Estimate")
+complete_totals_2020_pse <- kp_tidier %>%
+  filter(time_period == "2020",
+         indicator_new == "Population Size Estimate")
   
 ou_table_2020_pse <- as.data.frame(tabyl(complete_totals_2020_pse, area, population)) %>%
   pivot_longer(!area, names_to = "population", values_to = "n_estimates") %>%
   filter(n_estimates > 0) %>%
   group_by(area) %>%
-  summarize(n_kps = sum(n_estimates)) %>%
-  filter(n_kps >= 5)
+  summarize(n_kps = sum(n_estimates))
 
-# no oUs have complete data for all KPs in 2020 (otherwise, n_kps would be >= 5 
-# since there are 5 KPs in this dataset,
-# the most any OU has is 4 and those are ony available for Nicaragua, 
-# Guatemala, Philippines, Côte d'Ivoire, Zambia 
 
 # which OUS have complete coverage data for all KPs in 2020?
 complete_totals_2020_cov <- complete_totals_2020 %>%
-  filter(indicator_new == "Coverage of HIV prevention programmes")
+  filter(time_period == "2020",
+         indicator_new == "Coverage of HIV prevention programmes")
 
 ou_table_2020_cov <- as.data.frame(tabyl(complete_totals_2020_pse, area, population)) %>%
   pivot_longer(!area, names_to = "population", values_to = "n_estimates") %>%
   filter(n_estimates > 0) %>%
   group_by(area) %>%
-  summarize(n_kps = sum(n_estimates)) %>%
-  filter(n_kps >= 5)
+  summarize(n_kps = sum(n_estimates))
 
-# no oUs have complete data for all KPs in 2020 (otherwise, n_kps would be >= 5 
-# since there are 5 KPs in this dataset,
-# the most any OU has is 4 and those are ony available for Nicaragua, 
-# Guatemala, Philippines, Côte d'Ivoire, Zambia 
-
-# What about 2019?
-complete_totals_2019 <- kp_tidier %>%
-  filter(time_period == "2019", 
-         geo_level == "National",
-         subgroup %in% c("Total", "estimate"),
-         indicator_new %in% c("Population Size Estimate", 
-                              "Coverage of HIV prevention programmes"))
-
-# Which ous have complete PSE data for all KPs?
-complete_totals_2019_pse <- complete_totals_2019 %>%
-  filter(indicator_new == "Population Size Estimate")
-
-ou_table_2019_pse <- as.data.frame(tabyl(complete_totals_2019_pse, area, population)) %>%
-  pivot_longer(!area, names_to = "population", values_to = "n_estimates") %>%
-  filter(n_estimates > 0) %>%
-  group_by(area) %>%
-  summarize(n_kps = sum(n_estimates)) %>%
-  filter(n_kps == 1)
-
-# no oUs have complete data for all KPs in 2019 (otherwise, n_kps would be >= 5 
-# since there are 5 KPs in this dataset,
-# the most any OU has is 4 and those are only available for
-# Guatemala, Philippines, Singapore, Lao People's Democratic Republic,
-# Dominican Republic, Mexico
-
-# Let's say we want to make a scorecard for COD
-# What data are most recently available for that?
-
-kp_cod_recent <- kp_tidier %>%
-  filter(area_id == "COD", 
-         time_period == "2020")
-
-kp_table_1 <- tabyl(kp_cod_recent, population, indicator_new)
-
-# For the year 2020 in COD, we have data from the following KPs: 
-
-# "men who have sex with men", 5/6 indicators
-# "men who have sex with men living with HIV", 1/ 6 indicators
-# "people who inject drugs", 6/6 indicators
-# "prisoners", 2/6 indicators
-# "prisoners living with HIV"  , 1/ 6 indicators               
-# "sex workers", 6/6 indicators                              
-# "transgender people", 6/6 indicators
-
-# on the following indicators: 
-
-# Antiretroviral therapy coverage`, 5/7 pops listed above
-# Avoidance of health care because of stigma and discrimination`, 4/7 pops listed above
-# Coverage of HIV prevention programmes`, 4/7 pops listed above
-# HIV prevalence`, 5/7 pops listed above
-# HIV testing and status awareness`, 4/7 pops listed above
-# Population Size Estimate`, 5/7 pops listed above
-
-# Is this information available for all years?
-
-kp_cod_allyears <- kp_tidier %>%
-  filter(area_id == "COD")
-
-kp_table_2 <- tabyl(kp_cod_allyears, population, indicator_new, time_period)
-
-# data only available between 2011 and 2020, 
-# specifically only 2011, 2012, 2013, 2015, 2016, 2018, 2019, and 2020
-# available indicators also vary from year to year with data becoming 
-# more and more sparse as the years progress
-
-# draft figures ----------------------------------------------------------------
-  
-# population sizes: 
-# which years have population size estimates for each KP?
-cod_pop_data_allyrs <- kp_cod_allyears %>%
-  filter(indicator_new == "Population Size Estimate")
-
-cod_table <- tabyl(cod_pop_data_allyrs, time_period, population)
-# We only have data for 3 KPs for 3 of the same years 
-
-ggplot(cod_pop_data_allyrs, 
-       aes(y = data_value, 
-           x = time_period, 
-           fill = population)) +
-  geom_col() +
-  facet_wrap(~population) +
-  si_style_ygrid() +
-  scale_fill_manual(values = 
-  c("men who have sex with men" = moody_blue_light, 
-    "people who inject drugs" = scooter_light, 
-    "prisoners" = denim_light, 
-    "sex workers" = genoa_light), 
-    labels = NULL) +
-  theme(legend.position = "none") +
-  labs( 
-    x = NULL,
-    y = NULL,
-    fill = NULL, 
-    title = "Key population (KP) size estimates are unavailable or inconsistent from year to year",
-    subtitle = "Estimates are not available for all KPs of interest for all years, including most recent.",
-    caption = "Source: UNAIDS Key Population Atlas Database 2021 | refid: d1836592")
-
-si_save(here::here("thathill/Graphics/pop_size_ests_draft.png"))
-
-# prevention: 
-
-# what data do we have for prevention indicators?
-
-# How much data do we have in the prevention bucket?
-
-cod_prev_data <- kp_cod_allyears %>%
-  filter(bucket == "prevention",
-         subgroup %in% c("Total")) %>%
-  mutate(data_value = as.numeric(data_value)) %>%
-  filter(data_value > 0)
-
-# HIV prevalence, 2015 and 2019
-# only have matching estimates from 2015 for the groups shown, 
-# 2019 is the most recent year available for all groups
-# total or all ages, COD, 
-# only have data from 2020 for MSM, only have All ages data from 2019, no age breakdowns
-# don't have 2015 data for sex workers
-
-ggplot(cod_prev_data, 
-       aes(x = time_period, 
-           y = data_value, 
-           fill = population)) +
-  geom_col() +
-  facet_wrap(~population) +
-  si_style_ygrid() +
-  scale_fill_manual(values = 
-                      c("men who have sex with men" = moody_blue_light, 
-                        "people who inject drugs" = scooter_light, 
-                        "prisoners" = denim_light, 
-                        "sex workers" = genoa_light), 
-                    labels = NULL) +
-  theme(legend.position = "none") +
-  labs( 
-    x = NULL,
-    y = NULL,
-    fill = NULL, 
-    title = "HIV prevalence estimates are unavailable or inconsistent from year to year",
-    subtitle = "Estimates are not available for all KPs of interest for all years, including most recent.",
-    caption = "Source: UNAIDS Key Population Atlas Database 2021 | refid: d1836592")
-
-si_save(here::here("thathill/Graphics/hiv_prev_ests_draft.png"))
 
 # What if we look at prevention in the UNAIDS data? ----------------------------
 unaids_prev <- read_sheet("1yrgS_ZbA3Q8diICkQnl9CdF-77hF8PKuQ9KGP7r6vGg")
@@ -686,7 +478,6 @@ si_save("thathill/Graphics/prevention_hivprev.svg")
 
 # How has incidence per 100k changed over time in OUs with highest rates?
 
-
 unaids_prev_wide %>%
   ggplot(aes(x = year)) +
   geom_ribbon(aes(ymax = incidence_per_1000_lower_bound, 
@@ -722,6 +513,9 @@ si_save("thathill/Graphics/prevention_hivincper1000.svg")
 # kps?
 # heatmap of kp data by ous and year
 
+# draft figures ----------------------------------------------------------------
+
+
 # population size estimate
 sparse_df_pse <- populate_sparse_df_notPLHIV(df = kp_tidier, 
                                              indicator = "Population Size Estimate")
@@ -730,8 +524,8 @@ sparse_df_pse %>%
                        "How Many Poulation Size Estimates Exist From PEPFAR supported OUs
  with available KP data in the previous 5 years?")
 
-si_save(glue("Images/KPAtlasfindings_PopulationSizeEst_{Sys.Date()}.png"),
-        height = 10, width = 10)
+si_save(glue("thathill/Images/KPAtlasfindings_PopulationSizeEst_{Sys.Date()}.svg"),
+        height = 9, width = 16)
 
 # HIV prevalence
 sparse_df_hivprev <- populate_sparse_df_notPLHIV(df = kp_tidier, 
